c042fc97ed25ba2d5fae79145900c3bab90fdbc1
shahzad.arshad <vn52j9d@wal-mart.com>
2022-09-29 13:07:34 +0500

    initial setup

---
+++ xm-php-assignment/database/migrations/2022_09_27_165435_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique();
+            $table->integer('symbol_id');
+            $table->date('start_date')->format('Y-m-d');
+            $table->date('end_date')->format('Y-m-d');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
