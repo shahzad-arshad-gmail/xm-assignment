85f83c656d99d3629658fe6fc3d6f12a3cf7aef7
shahzad.arshad <vn52j9d@wal-mart.com>
2022-09-29 14:43:29 +0500

    send mail logic add

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
@@ -31,41 +32,73 @@ class CompanyController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
 
         $validator = Validator::make($request->all(), [
-            'email'=>'required|email|unique:companies',
-            'symbol_id'=>'required',
-            'start_date'=>'required',
-            'end_date'=>'required'
+            'email' => 'required|email|unique:companies',
+            'symbol_id' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required'
         ]);
 
         if ($validator->fails()) {
             return response()->json([
-                'errors'=>$validator->messages()
+                'errors' => $validator->messages()
             ]);
         }
 
-        try{
+        try {
             Company::create($request->post());
             return response()->json([
-                'message'=>'Company Created Successfully!!'
+                'message' => 'Company Created Successfully!!'
             ]);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return response()->json([
-                'message'=>$e->getMessage()
-            ],500);
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function sendEmail(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->messages()
+            ]);
+        }
+
+        try {
+
+            $to_name = 'XM';
+            $to_email = $request->email;
+            $data = array("name" => "Test", "body" => $request->startDate . ' ' . $request->endDate);
+            Mail::send('email', $data, function ($message) use ($to_name, $to_email, $request) {
+                $message->to($to_email, $to_name)
+                    ->subject('Company Symbol = ' . $request->companySymbol . ' => Company’s Name = Google');
+                $message->from('test@gmail.com', '');
+            });
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
         }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Company  $company
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
     public function show(Company $company)
@@ -76,7 +109,7 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Company  $company
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
     public function edit(Company $company)
@@ -87,8 +120,8 @@ class CompanyController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Company  $company
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Company $company)
@@ -99,7 +132,7 @@ class CompanyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Company  $company
+     * @param \App\Models\Company $company
      * @return \Illuminate\Http\Response
      */
     public function destroy(Company $company)
